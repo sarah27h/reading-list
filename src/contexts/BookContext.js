71e3fcf1ea5d6aeb8d55ecf17f453f19ca5b8f80
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2020-01-06 18:40:47 +0200

    Feat: add local storage

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,21 +1,19 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import { bookReducer } from '../reducers/bookReducer';
 export const BookContext = createContext();
 
 const BookContextProvider = props => {
-  const [books, dispatch] = useReducer(bookReducer, []);
-  // const [books, setBook] = useState([
-  //   { title: 'name of the wind', author: 'patrick rothfuss', id: 1 },
-  //   { title: 'the final empire', author: 'brandon sanderson', id: 2 }
-  // ]);
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const localData = localStorage.getItem('books');
+    console.log(localData);
+    return localData ? JSON.parse(localData) : [];
+  });
 
-  // const addBook = (title, author) => {
-  //   setBook([...books, { title, author, id: uuid() }]);
-  // };
-
-  // const removeBook = id => {
-  //   setBook([...books.filter(book => book.id !== id)]);
-  // };
+  // user enter new book will update books state, and then code in useEffect() will run
+  // add book in local storage in useEffect
+  useEffect(() => {
+    localStorage.setItem('books', JSON.stringify(books));
+  }, [books]);
 
   return <BookContext.Provider value={{ books, dispatch }}>{props.children}</BookContext.Provider>;
 };
