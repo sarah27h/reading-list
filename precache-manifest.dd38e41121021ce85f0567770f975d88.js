6db4b043d7c237cbd7cf3c80d98199cc3ca98a16
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2020-01-06 18:47:25 +0200

    Updates

---
+++ precache-manifest.dd38e41121021ce85f0567770f975d88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c3adbf7cb8ebabe3df621b19d93d8e",
+    "url": "/reading-list/index.html"
+  },
+  {
+    "revision": "b1b3728ba80e42fe95f4",
+    "url": "/reading-list/static/css/main.12ada5de.chunk.css"
+  },
+  {
+    "revision": "3c4a2c6d332d6eee9fe0",
+    "url": "/reading-list/static/js/2.3ab94eaf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reading-list/static/js/2.3ab94eaf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1b3728ba80e42fe95f4",
+    "url": "/reading-list/static/js/main.4050bf29.chunk.js"
+  },
+  {
+    "revision": "8c39874db1b3f4b4a467",
+    "url": "/reading-list/static/js/runtime-main.c5afa04d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reading-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
